8877a1e2aae43c1173c4f7099511a176665e9171
olivier karg <olivierkarg@oliviers-MacBook-Pro-2.local>
2018-10-26 13:32:23 +0200

    derde

--- src/common/database.py
+++ src/common/database.py
@@ -10,7 +10,7 @@ class Database(object):
     @staticmethod
     def initialize():
         client = pymongo.MongoClient(Database.URI)
-        Database.DATABASE = client['fullstack']
+        Database.DATABASE = client['nieuwer']
 
     @staticmethod
     def insert(collection, data):
